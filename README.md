e1d7168529b4e177f020729e77aede9a464735ba
hayakawa saki <hayakawa391nopasokon@gmail.com>
2026-03-09 19:32:07 +0900

    fix docker environment

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
-# Free Market App
-
 ## 環境構築手順
 
 ```bash
 git clone https://github.com/hayakawa391/free-market-app.git
 cd free-market-app
+
+# 環境ファイル作成
 cp src/.env.example src/.env
-docker-compose up -d --build
-docker-compose exec php bash
+
+# コンテナ起動
+docker compose up -d --build
+
+# ライブラリインストール
+docker compose exec php composer install
+
+# APP_KEY生成
+docker compose exec php php artisan key:generate
+
+# マイグレーション実行
+docker compose exec php php artisan migrate
+compose exec php bash
 composer install
 php artisan key:generate
 php artisan migrate --seed
---
+++ src/database/seeders/ItemSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Item;
+
+class ItemSeeder extends Seeder
+{
+    public function run()
+    {
+        Item::insert([
+            [
+                'name' => '腕時計',
+                'price' => 15000,
+                'brand' => 'Rolax',
+                'description' => 'スタイリッシュなデザインのメンズ腕時計',
+                'img_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Armani+Mens+Clock.jpg',
+                'status' => '良好',
+                'category' => 'ファッション',
+                'user_id' => 1,
+                'is_sold' => 0
+            ],
+            [
+                'name' => 'HDD',
+                'price' => 5000,
+                'brand' => '西芝',
+                'description' => '高速で信頼性の高いハードディスク',
+                'img_url' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/HDD+Hard+Disk.jpg',
+                'status' => '目立った傷や汚れなし',
+                'category' => '家電',
+                'user_id' => 1,
+                'is_sold' => 0
+            ]
+        ]);
+    }
+}
